1dea5b29a0b00c0190695fde8a2214a5b573b2fd
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2022-07-13 12:18:46 +0000

    Commited

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "../Main Components/Navigation";
+
+function Home() {
+  return (
+    <section>
+      <Navigation></Navigation>
+      <h1>Hi, I'm Ama Odoom</h1>
+      <h2>A Web Designer and a Front End Developer</h2>
+    </section>
+  );
+}
+export default Home;
