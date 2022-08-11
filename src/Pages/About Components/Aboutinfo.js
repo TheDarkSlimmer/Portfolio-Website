bef3207e7ee7cecd0d3f5081c091cd39ae11a6d8
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2022-08-11 09:13:15 +0000

    fixed props in paragraphs

--- src/Pages/About Components/Aboutinfo.js
+++ src/Pages/About Components/Aboutinfo.js
@@ -3,6 +3,7 @@ import AboutinfoStyles from "../About Components/Aboutinfo.module.css";
 function Aboutinfo(props) {
   return (
     <div className={AboutinfoStyles.aboutBody}>
+      <h1 className={AboutinfoStyles.header}>About me</h1>
       <div className={AboutinfoStyles.paragraphs}>
         <p>{props.paragraph}</p>
       </div>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -24,14 +24,16 @@ const aboutMeInfo = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
   },
 ];
+
+let paragraphs = aboutMeInfo.map((about) => <p>{about.paragraph}</p>);
+
+let uniqueKey = aboutMeInfo.map((key) => <p>{key.pID}</p>);
 function About(props) {
   return (
     <>
       <Navigation />
       <Skills />
-      {aboutMeInfo.map((AboutPara) => (
-        <Aboutinfo key={AboutPara.pID} paragraph={AboutPara.paragraph} />
-      ))}
+      <Aboutinfo key={uniqueKey} paragraph={paragraphs} />
     </>
   );
 }
