3ac6ddb1dea1bc1c702132b972955d1f2d8b0029
Ama Osaba Odoom <amaosabaodoom@gmail.com>
2022-08-17 12:07:08 +0000

    Added message button and styles

--- src/Pages/HomeCompoents/HomePage.js
+++ src/Pages/HomeCompoents/HomePage.js
@@ -1,17 +1,28 @@
 import homeStyles from "../HomeCompoents/HomePage.module.css";
 import myself from "../../Images/Me/SocialBook Cartoonizer Baby 3.jpeg";
+import MessagePop from "../Main Components/MessagePop";
+import MessageButton from "../Main Components/MessageButton";
 
 function HomePage(props) {
   return (
-    <div className={homeStyles.homeContainer}>
-      <img src={myself} className={homeStyles.myself} />
-      <div className={homeStyles.homeInfo}>
-        <h1>
-          Hi, <br /> I'm Ama Odoom
-        </h1>
-        <h1>A Web Designer and a Front End Developer</h1>
+    <>
+      <MessagePop />
+
+      <div className={homeStyles.homeContainer}>
+        <img
+          src={myself}
+          className={homeStyles.myself}
+          alt="Ama Odoom cartoon"
+        />
+        <div className={homeStyles.homeInfo}>
+          <h1>
+            Hi, <br /> I'm Ama Odoom
+          </h1>
+          <h1>A Web Designer and a Front End Developer</h1>
+        </div>
+        <MessageButton />
       </div>
-    </div>
+    </>
   );
 }
 export default HomePage;
--- src/Pages/Main Components/MessagePop.js
+++ src/Pages/Main Components/MessagePop.js
@@ -1,15 +1,21 @@
-import messageStyles from '../Main Components/MessagePop.module.css'
+import messageStyles from "../Main Components/MessagePop.module.css";
+import sendImg from "../../Images/icons8-send-64.png";
 
-export default function MessagePop(props){
-
-    return(
-        <form>
-            <h2 className={messageStyles.mHeader}>Get in Touch</h2>
-           <input type='text' placeholder="Name"/><br/>
-           <input type='email' placeholder="Email"/><br/>
-           <label>Message</label><br/>
-           <textarea></textarea><br/>
-           <button className={messageStyles.mButton}>Send</button>
-        </form>
-)
+export default function MessagePop(props) {
+  return (
+    <form className={messageStyles.MPform}>
+      <h2 className={messageStyles.mHeader}>Get in Touch</h2>
+      <input type="text" placeholder="Name" />
+      <br />
+      <input type="email" placeholder="Email" />
+      <br />
+      <label>Message</label>
+      <br />
+      <textarea></textarea>
+      <br />
+      <button className={messageStyles.mButton}>
+        Send <img src={sendImg} className={messageStyles.sendImg} alt="send icon" />
+      </button>
+    </form>
+  );
 }
